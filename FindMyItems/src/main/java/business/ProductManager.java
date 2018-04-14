4882e47ca1132e883373d9eb0009de46cd4ca62d
robinson <robinson@robinson-Lenovo-Yoga-2-Pro>
2018-04-14 13:32:23 +0200

    main

---
+++ FindMyItems/src/main/java/business/ProductManager.java
@@ -0,0 +1,29 @@
+package business;
+
+import modell.Owner;
+import modell.Product;
+
+public class ProductManager {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product products = new Product();
+		Owner proOwner = new Owner();
+		proOwner.setProduct(products);
+		proOwner.setName("David");
+		proOwner.setOwnerId((long) 12);
+		proOwner.setVorname("Robinson");
+		products.setProductDescription("White usb");
+	products.setProductId((long) 12);
+	products.setProductName("Cables");
+	products.setPurchseDate(null);
+		
+		
+		
+		System.out.println(proOwner.toString());
+
+	}
+
+}
